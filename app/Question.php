2f9520062f4cf9539c0b2c13483f3a4227e33e45
sita021 <jsitaric@gmail.com>
2019-10-04 19:13:17 +0200

    Generate Question model via artisan model generator

--- app/Question.php
+++ app/Question.php
@@ -6,5 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class Question extends Model
 {
-    //
+
+    protected $fillable = ['title'. 'body'];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+
+        $question = Question::find(1);
+    }
 }
